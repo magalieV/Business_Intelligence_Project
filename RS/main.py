fa2a4e9efa479e1ce8d80ae5d54fd45cdbca235a
BigTzu <joshuel.kuilwijk@epitech.eu>
2021-11-28 14:25:04 +0900

    Find matching group and multi group matching working

--- RS/main.py
+++ RS/main.py
@@ -4,7 +4,7 @@ from firebase_admin import credentials, firestore
 cred = credentials.Certificate("hallowed-welder-297014-042131c68dd6.json")
 firebase_admin.initialize_app(cred)
 
-db=firestore.client()
+db = firestore.client()
 
 
 def parse_interest(document_name):
@@ -16,18 +16,37 @@ def parse_interest(document_name):
 
 
 def find_right_group(interest_group_c, interest_group_r, interest_group_p, interest_group_i, user_interests):
-    groups = [set(interest_group_i), set(interest_group_p), set(interest_group_r), set(interest_group_c)]
+    groups_dict = {
+        "group_I": set(interest_group_i),
+        "group_P": set(interest_group_p),
+        "group_R": set(interest_group_r),
+        "group_C": set(interest_group_c)
+    }
     set_user = set(user_interests)
+    right_group = {
+        "group": [],
+        "value": []
+    }
 
-    for group in groups:
-        print(group.intersection(set_user))
+    for key, value in groups_dict.items():
+        if not right_group['group']:
+            right_group['group'].append(key)
+            right_group['value'].append(value.intersection(set_user))
+        elif len(right_group['value'][0]) < len(value.intersection(set_user)):
+            right_group['group'].clear()
+            right_group['value'].clear()
+            right_group['group'].append(key)
+            right_group['value'].append(value.intersection(set_user))
+        elif len(right_group['value'][0]) == len(value.intersection(set_user)):
+            right_group['group'].append(key)
+            right_group['value'].append(value.intersection(set_user))
+
+        print("Group : {}, Common interests : {}".format(key, value.intersection(set_user)))
+    print("\n// Match Groups : {}\n// Common interests : {}".format(right_group['group'], right_group['value']))
+
 
 def main():
-    interest_group_c = parse_interest('C')
-    interest_group_i = parse_interest('I')
-    interest_group_p = parse_interest('P')
-    interest_group_r = parse_interest('R')
-    user_interests = {
+    user_interestsA = {  # VARIABLE FOR TESTING PURPOSES
         "interest1": "1",
         "interest207": "1",
         "interest134": "1",
@@ -39,9 +58,24 @@ def main():
         "interest14": "1",
         "interest89": "1",
     }
-    find_right_group(interest_group_c, interest_group_r, interest_group_p, interest_group_i, user_interests)
+    user_interestsB = {  # VARIABLE FOR TESTING PURPOSES
+        "interest33": "1",
+        "interest12": "1",
+        "interest45": "1",
+        "interest153": "1",
+        "interest198": "1",
+        "interest109": "1",
+        "interest178": "1",
+        "interest200": "1",
+        "interest202": "1",
+        "interest217": "1",
+    }
+    interest_group_c = parse_interest('C')
+    interest_group_i = parse_interest('I')
+    interest_group_p = parse_interest('P')
+    interest_group_r = parse_interest('R')
+    find_right_group(interest_group_c, interest_group_r, interest_group_p, interest_group_i, user_interestsB)
 
 
 if __name__ == "__main__":
     main()
-
