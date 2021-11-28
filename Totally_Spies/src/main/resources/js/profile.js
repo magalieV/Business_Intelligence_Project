e85ef4bad56a4da3a35c940ab3379fb53aac2b24
irama <irama.chaouch@epitech.eu>
2021-11-29 04:09:18 +0900

    page profil refaite / nouveaux champs / boutton add + delet fonctionnel

---
+++ Totally_Spies/src/main/resources/js/profile.js
@@ -0,0 +1,10 @@
+/*function myFunction() {
+    /*document.getElementById("add-list").addEventListener("click", function() {
+        alert("Hello World!");
+      });
+      const element = document.querySelector("#add-list");
+
+    element.addEventListener("click", () => {
+	    //console.log("clicked element");
+});
+}*/
